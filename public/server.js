bf557700f88cce6e2c6c4475d253ba1577f08f6e
Adam Reed <thirdeyeadam@gmail.com>
2021-04-20 16:50:24 -0500

    adding html route, not working correctly

---
+++ public/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes')(app);
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+  
