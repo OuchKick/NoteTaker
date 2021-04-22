a3e35a3f8a2fa61ac11b78fa768ada31da1bb2bc
Adam Reed <thirdeyeadam@gmail.com>
2021-04-22 17:38:28 -0500

    changed pathing a little

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => { res.json(notes)});
