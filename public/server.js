b8c8c8d0a80692df217e6d4cf12479b2b52a4687
Adam Reed <thirdeyeadam@gmail.com>
2021-04-22 15:57:43 -0500

    Save button and edit button aren't working, notes page isn't matching example

--- public/server.js
+++ public/server.js
@@ -1,16 +1,52 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const path = require('path');
+const fs = require('fs');
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.json());
+app.use(express.static('public'));
 
-require('../public/routes/htmlRoutes')(app);
-// require('../public/routes/apiRoutes')(app);
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db.json'));
+});
+
+
+app.post("/api/notes", function(req, res) {
+  fs.readFile(path.join(__dirname, "/db.json"), function(err, res) {
+      if (err) {
+          console.log(err);
+      }
+      const notes = JSON.parse(res);
+      const noteRequest = req.body;
+      const newNoteId = notes.length + 1;
+      const newNote = {
+          id: newNoteId,
+          title: noteRequest.title,
+          text: noteRequest.text
+      };
+      notes.push(newNote);
+      res.json(newNote);
+      fs.writeFile(path.join(__dirname, "/db.json"), JSON.stringify(notes, null, 2), function(err) {
+          if (err) throw err;
+      });
+  });
+});
+
 
 
 app.listen(PORT, () => {
